3defce523662ccf822808333d11de80b62733cc8
Jorge G <jorge.gar@me.com>
2020-09-25 01:14:24 -0700

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Jorge Garcia on 9/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
