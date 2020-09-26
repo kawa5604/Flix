11a03738c888502c876fec52a385c71e9dc56147
Jorge G <jorge.gar@me.com>
2020-09-26 03:42:46 -0700

    Add LaunchScreen delay. Add TableView. Get API request, parse JSON. Save as dict. Pass dict to TableView. Tableview.refresh()

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -7,12 +7,65 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//        movies.count
+        
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//        the cell
 
+        let cell = UITableViewCell()
+        //        get the movie titles
+        let movie = movies[indexPath.row]
+        //        This is from the API
+        let title = movie["title"] as! String
+        cell.textLabel!.text = "\(title)"
+        
+        return cell
+        
+    }
+    
+//global variables in this scope are called properties
+//    array of dictionaries for movies
+    var movies = [[String:Any]]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        print("-------inside of movies view controller-------")
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            
+            self.movies = dataDictionary["results"] as! [[String:Any]]
+            self.tableView.reloadData()
+            print(dataDictionary)
+              // TODO: Get the array of movies
+              // TODO: Store the movies in a property to use elsewhere
+              // TODO: Reload your table view data
+            
+           }
+        }
+        task.resume()
     }
     
 
