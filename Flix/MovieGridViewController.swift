3c2e2b399b3a51506d36afad6649bb0b155a1cb4
Jorge G <jorge.gar@me.com>
2020-09-30 23:01:03 -0700

    Add network request to movie grid view controller

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class MovieGridViewController: UIViewController {
 
+    
+    // movies property
+    
+    var movies = [[String:Any]]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Download the comic book movies only from the API
+        
+        // Searched for movies similar to infinity war on the movieDb
+        let url = URL(string: "https://api.themoviedb.org/3/movie/299536/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+           // This will run when the network request returns
+           if let error = error {
+              print(error.localizedDescription)
+           } else if let data = data {
+              let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+            
+            self.movies = dataDictionary["results"] as! [[String:Any]]
+           }
+        }
+        task.resume()
+//            print(dataDictionary)
+              // TODO: Get the array of movies
+              // TODO: Store the movies in a property to use elsewhere
+              // TODO: Reload your table view data
     }
     
 
