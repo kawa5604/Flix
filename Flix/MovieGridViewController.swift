2e41f9a5c8b7ef010387d6659ba3a4b31e40d8bc
Jorge G <jorge.gar@me.com>
2020-10-01 00:52:57 -0700

    Fix layout for image on grid Update API query call

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -26,7 +26,7 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
         // Download the comic book movies only from the API
         
         // Searched for movies similar to infinity war on the movieDb
-        let url = URL(string: "https://api.themoviedb.org/3/movie/299536/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let url = URL(string: "https://api.themoviedb.org/3/movie/299536/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&page=3")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
