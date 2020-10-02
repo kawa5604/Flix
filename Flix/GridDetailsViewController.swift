a400bc762a4cacf1bab931caf41c2858641a3744
Jorge G <jorge.gar@me.com>
2020-10-02 02:49:09 -0700

    Add segue and grid detail view

---
+++ Flix/GridDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GridDetailsViewController.swift
+//  Flix
+//
+//  Created by Jorge Garcia on 10/2/20.
+//
+
+import UIKit
+
+class GridDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
